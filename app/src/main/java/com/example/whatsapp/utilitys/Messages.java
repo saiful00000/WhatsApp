da8fac7f05b19eed07cb09c32750614d923e1306
saiful00000 <saiful.comilla24@yahoo.com>
2020-01-22 22:52:55 +0600

    Messages class added

--- app/src/main/java/com/example/whatsapp/utilitys/Messages.java
+++ app/src/main/java/com/example/whatsapp/utilitys/Messages.java
@@ -2,5 +2,41 @@ package com.example.whatsapp.utilitys;
 
 public class Messages {
 
+    String from;
+    String message;
+    String type;
 
+    public Messages() {
+
+    }
+
+    public Messages(String from, String message, String type) {
+        this.from = from;
+        this.message = message;
+        this.type = type;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
